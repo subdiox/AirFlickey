cd3e0b3b588d999a52220e3f944a1f79c099ad71
subdiox <subdiox@gmail.com>
2020-01-05 22:26:22 +0900

    show cursor

--- Assets/FlickKeyboard.cs
+++ Assets/FlickKeyboard.cs
@@ -7,8 +7,7 @@ using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit;
 using TMPro;
 
-public class FlickKeyboard : MonoBehaviour
-{
+public class FlickKeyboard : MonoBehaviour {
     public TMP_Text textField;
     public GameObject keyA;
     public GameObject keyKA;
@@ -80,9 +79,13 @@ public class FlickKeyboard : MonoBehaviour
     private Color pressedColor = new Color(17.0f / 255.0f, 17.0f / 255.0f, 17.0f / 255.0f, 0.0f);
     private Color unpressedColor = new Color(51.0f / 255.0f, 51.0f / 255.0f, 51.0f / 255.0f, 0.0f);
 
+    private float timeStamp;
+	private bool cursor = false;
+	private string cursorChar = "";
+    private string text = "";
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         audioSource = GetComponent<AudioSource>();
         flickableKeys = new GameObject[] { keyA, keyKA, keySA, keyTA, keyNA, keyHA, keyMA, keyYA, keyRA, keyWA, keyMark };
         InteractionManager.InteractionSourceDetected += SourceDetected;
@@ -92,85 +95,79 @@ public class FlickKeyboard : MonoBehaviour
         InteractionManager.InteractionSourceReleased += SourceReleased;
     }
 
-    void SourceDetected(InteractionSourceDetectedEventArgs eventArgs)
-    {
+    void Update() {
+        if (Time.time - timeStamp >= 0.5) {
+            timeStamp = Time.time;
+            if (cursor == false) {
+                cursor = true;
+                cursorChar += "_";
+            } else {
+                cursor = false;
+                if (cursorChar.Length != 0) {
+                    cursorChar = cursorChar.Substring(0, cursorChar.Length - 1);
+                }
+            }
+        }
+        textField.text = text + cursorChar;
+    }
+
+    void SourceDetected(InteractionSourceDetectedEventArgs eventArgs) {
         Debug.Log("SourceDetected");
     }
 
-    bool IsFlickable(GameObject key)
-    {
-        foreach (GameObject flickableKey in flickableKeys)
-        {
-            if (key == flickableKey)
-            {
+    bool IsFlickable(GameObject key) {
+        foreach (GameObject flickableKey in flickableKeys) {
+            if (key == flickableKey) {
                 return true;
             }
         }
         return false;
     }
 
-    void SourceUpdated(InteractionSourceUpdatedEventArgs eventArgs)
-    {
+    void SourceUpdated(InteractionSourceUpdatedEventArgs eventArgs) {
         Debug.Log("SourceUpdated");
         eventArgs.state.sourcePose.TryGetPosition(out releasedPosition3);
         int direction = GetCurrentDirection();
-        if (direction == -1)
-        {
+        if (direction == -1) {
             RestoreKeys();
-        }
-        else if (direction == 0)
-        {
+        } else if (direction == 0) {
             RestoreKeys(false);
             keyPressed.GetComponentInChildren<TMP_Text>().text = GetNextChar(direction);
-        }
-        else if (IsFlickable(keyPressed))
-        {
+        } else if (IsFlickable(keyPressed)) {
             keyPressed.GetComponentInChildren<TMP_Text>().text = null;
             keyOverlay.GetComponentInChildren<TMP_Text>().text = GetNextChar(direction);
-            if (direction == 1)
-            {
+            if (direction == 1) {
                 keyOverlay.transform.position = keyPressed.transform.position + new Vector3(-0.08f, 0.0f, -0.01f);
-            }
-            else if (direction == 2)
-            {
+            } else if (direction == 2) {
                 keyOverlay.transform.position = keyPressed.transform.position + new Vector3(0.0f, 0.08f, -0.01f);
-            }
-            else if (direction == 3)
-            {
+            } else if (direction == 3) {
                 keyOverlay.transform.position = keyPressed.transform.position + new Vector3(0.08f, 0.0f, -0.01f);
-            }
-            else if (direction == 4)
-            {
+            } else if (direction == 4) {
                 keyOverlay.transform.position = keyPressed.transform.position + new Vector3(0.0f, -0.08f, -0.01f);
             }
             keyOverlay.SetActive(true);
         }
     }
 
-    void SourceLost(InteractionSourceLostEventArgs eventArgs)
-    {
+    void SourceLost(InteractionSourceLostEventArgs eventArgs) {
         Debug.Log("SourceLost");
-        if (keyPressed != null)
-        {
+        if (keyPressed != null) {
             eventArgs.state.sourcePose.TryGetPosition(out releasedPosition3);
             ProcessInput(GetCurrentDirection());
         }
     }
 
-    void SourcePressed(InteractionSourcePressedEventArgs eventArgs)
-    {
+    void SourcePressed(InteractionSourcePressedEventArgs eventArgs) {
         Debug.Log($"SourcePressed {CoreServices.InputSystem.GazeProvider.GazeTarget}");
         eventArgs.state.sourcePose.TryGetPosition(out pressedPosition3);
         keyPressed = CoreServices.InputSystem.GazeProvider.GazeTarget;
-        if (keyPressed != null)
-        {
+        if (keyPressed != null) {
             audioSource.PlayOneShot(buttonPress, 1.0f);
             keyPressed.GetComponentInChildren<MeshRenderer>().material.color = pressedColor;
         }
     }
 
-    void SourceReleased(InteractionSourceReleasedEventArgs eventArgs)
-    {
+    void SourceReleased(InteractionSourceReleasedEventArgs eventArgs) {
         Debug.Log($"SourceReleased {keyPressed.name}");
         eventArgs.state.sourcePose.TryGetPosition(out releasedPosition3);
         ProcessInput(GetCurrentDirection());
@@ -185,35 +182,24 @@ public class FlickKeyboard : MonoBehaviour
         3 : 現在位置がキーをタップした位置から右方向にずれたと判定されるとき
         4 : 現在位置がキーをタップした位置から下方向にずれたと判定されるとき
     */
-    int GetCurrentDirection()
-    {
+    int GetCurrentDirection() {
         int direction = -1;
-        if (keyPressed != null)
-        {
+        if (keyPressed != null) {
             Vector2 relativePosition2 = releasedPosition3 - pressedPosition3;
             float angle = Vector2.Angle(Vector2.right, relativePosition2);
             Debug.Log($"relativePosition magnitude: {relativePosition2.magnitude}");
-            if (relativePosition2.magnitude < 0.02) // フリックしなかったと判定する基準
-            {
+            if (relativePosition2.magnitude < 0.02) { // フリックしなかったと判定する基準
                 direction = 0;
-            }
-            else if (angle < 45)
-            {
+            } else if (angle < 45) {
                 Debug.Log("Direction: right");
                 direction = 3;
-            }
-            else if (angle > 135)
-            {
+            } else if (angle > 135) {
                 Debug.Log("Direction: left");
                 direction = 1;
-            }
-            else if (relativePosition2.y > 0)
-            {
+            } else if (relativePosition2.y > 0) {
                 Debug.Log("Direction: up");
                 direction = 2;
-            }
-            else
-            {
+            } else {
                 Debug.Log("Direction: down");
                 direction = 4;
             }
@@ -231,124 +217,81 @@ public class FlickKeyboard : MonoBehaviour
         3 : キーをタップして右にフリックして放したときに入力される文字を返す
         4 : キーをタップして下にフリックして放したときに入力される文字を返す
     */
-    string GetNextChar(int direction)
-    {
-        if (direction >= -1 && direction <= 4)
-        {
-            if (keyPressed == keyA)
-            {
+    string GetNextChar(int direction) {
+        if (direction >= -1 && direction <= 4) {
+            if (keyPressed == keyA) {
                 return charListA[direction + 1];
-            }
-            else if (keyPressed == keyKA)
-            {
+            } else if (keyPressed == keyKA) {
                 return charListKA[direction + 1];
-            }
-            else if (keyPressed == keySA)
-            {
+            } else if (keyPressed == keySA) {
                 return charListSA[direction + 1];
-            }
-            else if (keyPressed == keyTA)
-            {
+            } else if (keyPressed == keyTA) {
                 return charListTA[direction + 1];
-            }
-            else if (keyPressed == keyNA)
-            {
+            } else if (keyPressed == keyNA) {
                 return charListNA[direction + 1];
-            }
-            else if (keyPressed == keyHA)
-            {
+            } else if (keyPressed == keyHA) {
                 return charListHA[direction + 1];
-            }
-            else if (keyPressed == keyMA)
-            {
+            } else if (keyPressed == keyMA) {
                 return charListMA[direction + 1];
-            }
-            else if (keyPressed == keyYA)
-            {
+            } else if (keyPressed == keyYA) {
                 return charListYA[direction + 1];
-            }
-            else if (keyPressed == keyRA)
-            {
+            } else if (keyPressed == keyRA) {
                 return charListRA[direction + 1];
-            }
-            else if (keyPressed == keyWA)
-            {
+            } else if (keyPressed == keyWA) {
                 return charListWA[direction + 1];
-            }
-            else if (keyPressed == keyMark)
-            {
+            } else if (keyPressed == keyMark) {
                 return charListMark[direction + 1];
             }
         }
         return keyPressed.GetComponentInChildren<TMP_Text>().text;
     }
 
-    void ProcessInput(int direction)
-    {
+    void ProcessInput(int direction) {
         audioSource.PlayOneShot(buttonUnpress, 1.0f);
-        if (keyPressed == keySpace)
-        {
-            textField.text += " ";
-        }
-        else if (keyPressed == keyDelete)
-        {
-            if (!string.IsNullOrEmpty(textField.text))
-            {
-                textField.text = textField.text.Substring(0, textField.text.Length - 1);
+        if (keyPressed == keySpace) {
+            text += " ";
+        } else if (keyPressed == keyDelete) {
+            if (!string.IsNullOrEmpty(text)) {
+                text = text.Substring(0, text.Length - 1);
             }
-        }
-        else if (keyPressed == keyReturn)
-        {
-            textField.text += "\n";
-        }
-        else if (keyPressed == keyFunc)
-        {
-            string targetChar = textField.text.Substring(textField.text.Length - 1, 1);
-            for (int i = 0; i < charListFunc.Length; i ++)
-            {
-                for (int j = 0; j < 3; j ++)
-                {
-                    if (charListFunc[i][j] == targetChar)
-                    {
-                        int k = (j + 1) % 3;
-                        while (charListFunc[i][k] == null)
-                        {
-                            k = (k + 1) % 3;
+        } else if (keyPressed == keyReturn) {
+            text += "\n";
+        } else if (keyPressed == keyFunc) {
+            string targetChar = text.Substring(text.Length - 1, 1);
+            if (targetChar != null) {
+                for (int i = 0; i < charListFunc.Length; i ++) {
+                    for (int j = 0; j < 3; j ++) {
+                        if (charListFunc[i][j] == targetChar) {
+                            int k = (j + 1) % 3;
+                            while (charListFunc[i][k] == null) {
+                                k = (k + 1) % 3;
+                            }
+                            text = text.Substring(0, text.Length - 1) + charListFunc[i][k];
+                            RestoreKeys();
+                            return;
                         }
-                        textField.text = textField.text.Substring(0, textField.text.Length - 1) + charListFunc[i][k];
-                        RestoreKeys();
-                        return;
                     }
                 }
             }
-        }
-        else
-        {
+        } else {
             string nextChar = GetNextChar(direction);
-            if (nextChar != null)
-            {
-                textField.text += nextChar;
+            if (nextChar != null) {
+                text += nextChar;
             }
         }
         RestoreKeys();
     }
 
-    void RestoreKeys(bool isDefault = true)
-    {
-        if (keyOverlay != null)
-        {
+    void RestoreKeys(bool isDefault = true) {
+        if (keyOverlay != null) {
             keyOverlay.SetActive(false);
         }
-        if (keyPressed != null)
-        {
-            if (isDefault)
-            {
+        if (keyPressed != null) {
+            if (isDefault) {
                 keyPressed.GetComponentInChildren<TMP_Text>().text = GetNextChar(-1);
                 keyPressed.GetComponentInChildren<MeshRenderer>().material.color = unpressedColor;
                 keyPressed = null;
-            }
-            else
-            {
+            } else {
                 keyPressed.GetComponentInChildren<TMP_Text>().text = GetNextChar(0);
             }
         }
